39e55ba4681751a0f8936a79f2b6f5f497b4e248
Asher Morgan <asher@ashermorgan.net>
2026-09-18 18:22:59 -0700

    Fix unit test failures when .env file exists

--- tests/unit/__init__.py
+++ tests/unit/__init__.py
@@ -1,4 +1,8 @@
+import os
 import unittest
+
+# Prevent env issues in TestPlaybookGetPlaybook.test_default_values() test
+os.environ['AOH_PLAYBOOKS_FILE'] = 'playbooks.yml'
 
 from .test_playbook import *
 from .test_security import *
